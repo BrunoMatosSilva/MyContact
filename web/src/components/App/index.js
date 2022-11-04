a0f9405dd509d568735338a8b67476091416269f
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-11-04 18:36:48 -0300

    criação do toast com eventListener

--- web/src/components/App/index.js
+++ web/src/components/App/index.js
@@ -6,12 +6,15 @@ import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import { Header } from '../Header';
 import { Container } from './styles';
+import ToastContainer from '../Toast/ToastContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
+        <ToastContainer />
+
         <Container>
           <Header />
           <Routes />
--- web/src/pages/NewContact/index.js
+++ web/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import ContactForm from "../../components/ContactForm";
 import PageHeader from "../../components/PageHeader";
 import ContactsService from "../../services/ContactsService";
+import toast from "../../utils/toast";
 
 export default function NewContact(){
   async function handleSubmit(formData) {
@@ -13,12 +14,18 @@ export default function NewContact(){
         category_id: formData.categoryId,
       };
 
-      const response = await ContactsService.createContacts(contact);
+     await ContactsService.createContacts(contact);
 
-      console.log(response);
+      toast({
+        type: 'success',
+        text: 'Contato cadastrado com sucesso!',
+      })
 
     }catch(error){
-      alert(error, 'Ocorreu um erro ao cadastrar contato!')
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar contato!',
+      })
     }
   }
   return (
