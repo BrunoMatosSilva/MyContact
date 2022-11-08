9ac285e6da3127a3f0fe28171d08fa84f3331c33
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-11-08 18:11:10 -0300

    remove toast com click encima

--- web/src/components/Toast/ToastContainer/index.js
+++ web/src/components/Toast/ToastContainer/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { toastEventManager } from "../../../utils/toast";
 import ToastMessage from "../ToastMessage";
 import { Container } from "./styles";
 
@@ -6,25 +7,34 @@ export default function ToastContainer(){
   const [messages, setMessages] = useState([])
 
   useEffect(() => {
-    function handleAddToast(event){
-      const {type, text} = event.detail;
+    function handleAddToast({type, text}){
 
       setMessages((prevState) => [
         ...prevState,
         {id: Math.random(), type, text},
       ]);
     }
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
 
     return () => {
-    document.removeEventListener('addtoast', handleAddToast);
+    toastEventManager.removeListener('addtoast', handleAddToast);
   }
 },[]);
+
+function handleRemoveMessage(id){
+  setMessages((prevState) => prevState.filter(
+    (message) => message.id !== id,
+  ));
+}
 
   return (
     <Container>
       {messages.map((message) => (
-        <ToastMessage key={message.id} text={message.text} type={message.type} />
+        <ToastMessage
+        key={message.id}
+        message={message}
+        onRemoveMessage={handleRemoveMessage}
+         />
       ))}
     </Container>
   )
--- web/src/components/Toast/ToastMessage/styles.js
+++ web/src/components/Toast/ToastMessage/styles.js
@@ -18,10 +18,10 @@ color: #fff;
 border-radius: 4px;
 box-shadow: 0px 20px 20px -16px rgba(0, 0, 0, 0.25);
 ${({type}) => containerVariants[type] || containerVariants.default}
-
 display: flex;
 align-items: center;
 justify-content: center;
+cursor: pointer;
 
 & + & {
   margin-top: 12px;
